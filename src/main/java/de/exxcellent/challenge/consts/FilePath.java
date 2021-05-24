4b2aa0f42322e1b29203edad0b28b3dcb651fe4b
RobinSzekely <robinsz@web.de>
2021-05-24 15:13:54 +0200

    feat: Added football.csv to FilePaths and FOOTBALL to FileContext enum

--- src/main/java/de/exxcellent/challenge/consts/FilePath.java
+++ src/main/java/de/exxcellent/challenge/consts/FilePath.java
@@ -5,6 +5,7 @@ package de.exxcellent.challenge.consts;
  */
 public class FilePath {
     public static final String WeatherPath = "src/main/resources/de/exxcellent/challenge/weather.csv";
+    public static final String FootballPath = "src/main/resources/de/exxcellent/challenge/football.csv";
 
     /* for tests */
     public static final String EmptyFilePath = "src/main/resources/de/exxcellent/challenge/test/empty.csv";
