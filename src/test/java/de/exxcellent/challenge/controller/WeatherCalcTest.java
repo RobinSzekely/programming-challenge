a2de413fdb314db4589b5970299560ec7fc479e2
RobinSzekely <robinsz@web.de>
2021-05-24 15:07:53 +0200

    test: JUnit test for WeatherCalc class, including test routine for correct result (day 14)

---
+++ src/test/java/de/exxcellent/challenge/controller/WeatherCalcTest.java
@@ -0,0 +1,33 @@
+package de.exxcellent.challenge.controller;
+
+import de.exxcellent.challenge.consts.FilePath;
+import de.exxcellent.challenge.enums.FileContext;
+import de.exxcellent.challenge.models.FileObject;
+import de.exxcellent.challenge.models.Weather;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherCalcTest {
+    private final AppController appController = new AppController();
+    private ArrayList<Weather> weatherList;
+
+    @BeforeEach
+    public void setUp() {
+        ArrayList<FileObject> rows = appController.generateModelList(FilePath.WeatherPath, FileContext.WEATHER);
+        weatherList = rows.stream().map(weather -> (Weather) weather).
+                collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    @Test
+    @DisplayName("Test weather calculation with null object")
+    public void smallestTemperatureSpreadTest() {
+        int result = WeatherCalc.dayWithSmallestTemperatureSpread(weatherList);
+        assertEquals(14, result);
+    }
+}
