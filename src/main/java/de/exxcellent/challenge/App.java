283e70394b362762ae53fa54869ed4b42369cf63
RobinSzekely <robinsz@web.de>
2021-05-24 15:11:42 +0200

    refactor: Implementation of weather calculation (day with smallest temperature spread) in App.java

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,15 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.consts.FilePath;
+import de.exxcellent.challenge.controller.AppController;
+import de.exxcellent.challenge.controller.WeatherCalc;
+import de.exxcellent.challenge.enums.FileContext;
+import de.exxcellent.challenge.models.FileObject;
+import de.exxcellent.challenge.models.Weather;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -13,10 +23,15 @@ public final class App {
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
+        /* Use AppController instance to create ArrayList of Weather objects */
+        AppController controller = new AppController();
+        ArrayList<FileObject> fileObjectList = controller.generateModelList(FilePath.WeatherPath,
+                FileContext.WEATHER);
+        ArrayList<Weather> weatherList = fileObjectList.stream().map(weather -> (Weather) weather).
+                collect(Collectors.toCollection(ArrayList::new));
 
-        // Your preparation code …
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        /* Output for Weather objects */
+        String dayWithSmallestTempSpread = ""+ WeatherCalc.dayWithSmallestTemperatureSpread(weatherList);
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
